ca5d6cd9338f4f7c635b54aa69c53cb67c5f02c4
egmkang wang <egmkang@gmail.com>
2013-07-12 17:12:00 +0800

    bug fix

--- green_turtle/blocking_queue.h
+++ green_turtle/blocking_queue.h
@@ -37,6 +37,7 @@
 
 namespace green_turtle{
 
+//support multi thread read and write
 template<class T>
 class BlockingQueue : NonCopyable
 {
@@ -46,39 +47,33 @@ class BlockingQueue : NonCopyable
   bool Push(value_type&& v)
   {
     std::lock_guard<std::mutex> guard(mutex_);
-    queue_write_.push_back(v);
+    queue_.push_back(v);
     return true;
   }
   bool Push(const value_type& v)
   {
     std::lock_guard<std::mutex> guard(mutex_);
-    queue_write_.push_back(v);
+    queue_.push_back(v);
     return true;
   }
 
   bool Pop(value_type& v)
   {
-    if(queue_read_.empty() && !queue_write_.empty())
+    std::lock_guard<std::mutex> guard(mutex_);
+    if(!queue_.empty())
     {
-      std::lock_guard<std::mutex> guard(mutex_);
-      std::swap(queue_read_, queue_write_);
-    }
-    if(!queue_read_.empty())
-    {
-      v = queue_read_.front();
-      queue_read_.pop_front();
+      v = queue_.front();
+      queue_.pop_front();
       return true;
     }
     return false;
   }
   uint64_t Size() const
   {
-    //unsafe
-    return queue_read_.size() + queue_write_.size();
+    return queue_.size();
   }
  private:
-  std::deque<value_type>  queue_write_;
-  std::deque<value_type>  queue_read_;
+  std::deque<value_type>  queue_;
   std::mutex              mutex_;
 };
 
